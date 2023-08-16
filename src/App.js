d6ff68e7b04d08f6dc3c363d4994ee13d3c80c35
Athens410 <athenah.coucean@gmail.com>
2023-08-16 15:52:22 -0400

    imported font awesome
    and GitHub and linked in fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function App() {
   return (
@@ -7,7 +9,34 @@ export default function App() {
       <header className="App-header">
        <button type="button" class="btn btn-primary">Primary</button>
       </header>
+<footer class="foot">⚡️👩🏽‍💻This website was coded by, Athenah Coucean</footer>
+<ul class="nav justify-content-center mt-5">
+    <li class="nav-item">
+        <a class="nav-link active" href="https://www.shecodes.io/" target="_blank"  rel="noreferrer" title="SheCodes profile">SheCodes</a>
+    </li>
+    
+    <li class="nav-item">
+        <a class="nav-link" href="https://linkedin.com/in/athenah-c-2639251a0" target="_blank" rel="noreferrer" title="linkedin profile">  <FontAwesomeIcon icon={faLinkedin} /></a>
+    </li>
+<li>
+    <a class="nav-link" href="https://github.com/Athens410/Portfolio-Project-Git.git" target="_blank" rel="noreferrer" title="github profile">   <FontAwesomeIcon icon={faGithub} /></a>
+    </li>
+<li class="nav-item ml-5">
+        <a class="nav-link"   href="https://www.shecodes.io/" target="_blank"  rel="noreferrer" tabindex="-1" aria-disabled="true">© 2021 SheCodes All rights reserved.</a>
+    </li>
+
+</ul>
+
+    
     </div>
+
+
+
+
+
+
+
+
   );
 }
 
